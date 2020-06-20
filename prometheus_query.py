02bccae2ede2f9faddb12163b2a9f8a98590441d
maygy.chang <maygy.chang@prophetstor.com>
2020-06-20 02:57:41 -0400

    fix logoffset and currentoffset

--- prometheus_query.py
+++ prometheus_query.py
@@ -44,8 +44,8 @@ class Prometheus_Query:
         output = self.p.run_cmd(cmd)
         return output
 
-    def query_log_offset(self, duration):
-        cmd = 'kafka_topic_partition_current_offset{topic=~"%s"}[%s]' % (topic_name, duration)
+    def query_log_offset(self):
+        cmd = 'sum(kafka_topic_partition_current_offset{topic=~"%s"})' % (topic_name)
         output = self.p.run_cmd(cmd)
         return output
 
@@ -59,8 +59,8 @@ class Prometheus_Query:
         output = self.p.run_cmd(cmd)
         return output
 
-    def query_current_offset(self, duration):
-        cmd = 'kafka_consumergroup_current_offset{topic=~"%s"}[%s]' % (topic_name, duration)
+    def query_current_offset(self):
+        cmd = 'sum(kafka_consumergroup_current_offset{topic=~"%s"})' % (topic_name)
         output = self.p.run_cmd(cmd)
         return output
 
@@ -123,47 +123,6 @@ def sum_offset_data(offset_output):
     return offset_list
 
 
-def calculate_lag(log_offset_list, current_offset_list):
-    lag = 0
-    log_offset = 0
-    last_logoffset_timestamp = 0
-    lag_flag = False
-    if log_offset_list:
-        last_logoffset_timestamp = sorted(log_offset_list.keys())[-1]
-        log_offset = log_offset_list[last_logoffset_timestamp]
-
-    current_offset = 0
-    last_current_timestamp = 0
-    if current_offset_list:
-        last_current_timestamp = sorted(current_offset_list.keys())[-1]
-        current_offset = current_offset_list[last_current_timestamp]
-
-    # print "log offset=", last_logoffset_timestamp, log_offset, "current offset=", last_current_timestamp, current_offset
-    if last_logoffset_timestamp >= last_current_timestamp and log_offset >= current_offset:
-        lag = log_offset - current_offset
-    else:
-        lag_flag = True
-        # print "current offset > log offset, use extrapolation to get lag"
-        if len(log_offset_list.keys()) > 1:
-            last_two_logoffset_timestamp = sorted(log_offset_list.keys())[-2]
-            old_log_offset = log_offset_list[last_two_logoffset_timestamp]
-            timestamp_diff = last_logoffset_timestamp - last_two_logoffset_timestamp
-            logoffset_diff = log_offset_list[last_logoffset_timestamp] - log_offset_list[last_two_logoffset_timestamp]
-            vary = logoffset_diff/(timestamp_diff*1.0)
-            x = (last_current_timestamp - last_two_logoffset_timestamp)/(timestamp_diff*1.0)
-            if last_current_timestamp != last_logoffset_timestamp:
-                log_offset = x * vary + old_log_offset
-            else:
-                log_offset = current_offset
-            # print "old log offset =", old_log_offset, last_two_logoffset_timestamp
-            # print "timestamp_diff=", timestamp_diff, "logoffset_diff=", logoffset_diff, "vary=", vary, "x=", x
-            # print "new log offset =", log_offset
-            lag = log_offset - current_offset
-    # if lag < 0:
-    #    raise Exception("Lag(%s) should not be less than 0" % lag)
-    return log_offset, current_offset, lag, lag_flag
-
-
 if __name__ == "__main__":
     algo_name = sys.argv[1]
     time_value = query_interval
@@ -171,11 +130,8 @@ if __name__ == "__main__":
 
     try:
         pq = Prometheus_Query()
-        log_offset_output = pq.query_log_offset("1m")
-        log_offset_list = sum_offset_data(log_offset_output)
-        current_offset_output = pq.query_current_offset("1m")
-        current_offset_list = sum_offset_data(current_offset_output)
-        log_offset, current_offset, lag, lag_flag = calculate_lag(log_offset_list, current_offset_list)
+        log_offset = pq.query_log_offset().get("data", {}).get("result", [])[0].get("value")[1]
+        current_offset = pq.query_current_offset().get("data", {}).get("result", [])[0].get("value")[1]
         lag = 0
         if pq.query_lag().get("data", {}).get("result", []):
             lag = pq.query_lag().get("data", {}).get("result", [])[0].get("value")[1]
